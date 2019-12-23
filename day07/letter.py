5418f62c488ac3174bb10c377b4708ab1e3a422c
marxin <mliska@suse.cz>
2019-12-23 22:07:43 +0100

    Finish part 2.

--- day07/letter.py
+++ day07/letter.py
@@ -2,7 +2,10 @@
 
 lines = open('input.txt').readlines()
 
+delta = 60
+N = 5
 dependencies = {}
+workers = [None] * N
 
 for l in lines:
     parts = l.split(' ')
@@ -14,23 +17,40 @@ for l in lines:
     if not needed in dependencies:
         dependencies[needed] = set()
 
-def make_step():
-    global result
+def get_available_letters():
     if not len(dependencies):
         return None
-    candidates = [(k, v) for k, v in dependencies.items() if not len(v)]
-    selected = sorted(candidates, key = lambda x: x[0])[0]
-    letter = selected[0]
+    candidates = [k for k, v in dependencies.items() if not len(v)]
+    return list(sorted(candidates))
+
+def letter_done(letter):
+    del dependencies[letter]
     for k, v in dependencies.items():
         v.discard(letter)
-    del dependencies[letter]
-    return letter
 
-result = ''
-while True:
-    s = make_step()
-    if s == None:
-        print(result)
-        break
-    else:
-        result += s
+doing = set()
+print(dependencies)
+for t in range(1100):
+    for w in range(N):
+        if workers[w] and workers[w][1] == t:
+            # job is done
+            print('Worker #%d finished %s at %d' % (w, workers[w][0], t))
+            letter_done(workers[w][0])
+            doing.remove(workers[w][0])
+            workers[w] = None
+
+    available = [x for x in get_available_letters() if not x[0] in doing]
+    for w in range(N):
+        if workers[w] == None:
+            # assign the job to worker
+            if len(available):
+                job = available[0]
+                duration = delta + ord(job) - ord('A') + 1 + t
+                print('Worker #%d starting %s at %d (will finish at %d)' % (w, job, t, duration))
+                workers[w] = (job, duration)
+                doing.add(job)
+                available = available[1:]
+            else:
+                break
+
+print(dependencies)
